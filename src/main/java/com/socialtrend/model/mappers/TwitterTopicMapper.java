3a7da1f689e4bee8cda661e77ea364ee917924be
Miguel Angel Escribano <maescribano85@gmail.com>
2016-07-27 22:14:05 +0200

    feat(TwitterTopicMapper): add code to map from twitter json response to topic object

---
+++ src/main/java/com/socialtrend/model/mappers/TwitterTopicMapper.java
@@ -0,0 +1,37 @@
+package com.socialtrend.model.mappers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.socialtrend.model.Topic;
+
+@Component
+public class TwitterTopicMapper {
+		
+	public Topic map(JsonObject jsonTwitterTopic){
+		final Topic twitterTopic = new Topic();
+		
+		twitterTopic.setDescription(jsonTwitterTopic.get("text").getAsString());
+		twitterTopic.setPreviewImageUrl(jsonTwitterTopic.get("entities").getAsJsonObject().get("media").getAsJsonArray().get(0).getAsJsonObject().get("media_url").getAsString());
+		twitterTopic.setShowContentUrl(jsonTwitterTopic.get("entities").getAsJsonObject().get("media").getAsJsonArray().get(0).getAsJsonObject().get("url").getAsString());
+		twitterTopic.setTitle(jsonTwitterTopic.get("text").getAsString());
+		
+		return twitterTopic;
+	}
+	
+	public List<Topic> map (JsonArray jsonTwitterTopics){
+		final List<Topic> twitterTopics = new ArrayList<Topic>();			
+		
+		for(final JsonElement twitterTopic : jsonTwitterTopics) {
+		    Topic topic =  map((JsonObject)twitterTopic);
+		    twitterTopics.add(topic);
+		}
+	
+		return twitterTopics;
+	}
+}
---
+++ src/test/java/com/socialtrend/model/mappers/TwitterTopicMapperTest.java
@@ -0,0 +1,66 @@
+package com.socialtrend.model.mappers;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import com.socialtrend.config.BeansConfig;
+import com.socialtrend.model.Topic;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { BeansConfig.class})
+public class TwitterTopicMapperTest {
+
+	JsonParser jsonParser;
+	JsonReader jsonReader;
+	JsonObject simpleJsonObjectMock;
+	
+	@Autowired
+	private TwitterTopicMapper twitterTopicMapper;
+
+
+	@Before
+	public void init() throws FileNotFoundException {
+		jsonParser = new JsonParser();
+		JsonReader jsonReader = new JsonReader(new FileReader("src/test/resources/MockTwitterResponse.json"));
+		simpleJsonObjectMock = jsonParser.parse(jsonReader).getAsJsonObject();
+	}
+
+	@Test
+	public void simpleJsonObjectMapperTest(){
+		final JsonObject firstTwitterTopic = simpleJsonObjectMock.get("statuses").getAsJsonArray().get(0).getAsJsonObject();
+		Topic twitterTopic = twitterTopicMapper.map(firstTwitterTopic);
+		
+		assertTrue(firstTwitterTopic.get("text").getAsString().equals(twitterTopic.getDescription()));
+		assertTrue(firstTwitterTopic.get("entities").getAsJsonObject().get("media").getAsJsonArray().get(0).getAsJsonObject().get("media_url").getAsString().equals(twitterTopic.getPreviewImageUrl()));
+		assertTrue(firstTwitterTopic.get("entities").getAsJsonObject().get("media").getAsJsonArray().get(0).getAsJsonObject().get("url").getAsString().equals(twitterTopic.getShowContentUrl()));
+		assertTrue(firstTwitterTopic.get("text").getAsString().equals(twitterTopic.getTitle()));
+
+	}
+	
+	
+	@Test
+	public void JsonObjectCollectionMapperTest(){
+		final JsonArray jsonTwitterTopics = simpleJsonObjectMock.get("statuses").getAsJsonArray();
+		List<Topic> twitterTopics = twitterTopicMapper.map(jsonTwitterTopics);		
+
+		assertTrue((jsonTwitterTopics.get(0).getAsJsonObject().get("text").getAsString().equals(twitterTopics.get(0).getDescription())));
+		assertTrue((jsonTwitterTopics.get(0).getAsJsonObject().get("entities").getAsJsonObject().get("media").getAsJsonArray().get(0).getAsJsonObject().get("media_url").getAsString().equals(twitterTopics.get(0).getPreviewImageUrl())));
+		assertTrue(jsonTwitterTopics.get(0).getAsJsonObject().get("entities").getAsJsonObject().get("media").getAsJsonArray().get(0).getAsJsonObject().get("url").getAsString().equals(twitterTopics.get(0).getShowContentUrl()));
+		assertTrue(jsonTwitterTopics.get(0).getAsJsonObject().get("text").getAsString().equals(twitterTopics.get(0).getTitle()));
+	}
+
+}
