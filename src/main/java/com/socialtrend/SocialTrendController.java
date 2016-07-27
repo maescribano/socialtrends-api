601873f8d8eab92998e1925216904a5c48aa7ba8
Miguel Angel Escribano <maescribano85@gmail.com>
2016-07-27 20:33:45 +0200

    feat(trendingtopic): add trendingtopic endpoint implementation

--- src/main/java/com/socialtrend/SocialTrendController.java
+++ src/main/java/com/socialtrend/SocialTrendController.java
@@ -1,6 +1,6 @@
 package com.socialtrend;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,42 +9,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.socialtrend.config.Constants;
 import com.socialtrend.model.Topic;
-import com.socialtrend.services.TrendingInfoRetriever;
 import com.socialtrend.services.impl.TrendingTopicCollector;
 
 @RestController
 @RequestMapping(path="/socialtrends")
-public class SocialTrendController {
-	
-	final static String TP_CLOSEST_REQUEST_TYPE = "closest";
-	final static String TP_WORLD_REQUEST_TYPE = "world";
-	
+public class SocialTrendController {	
 	
 	@Autowired
 	TrendingTopicCollector trendingTopicCollector;
 
 	//TODO: incluir param socialnets, de momento pide todas las que esten en la lista de retrievers
+	//TODO: incluir otro endpoint /topic para buscar topics por nombre no necesariamente los mas trendy.
 	@RequestMapping(path="/trendingtopic")
-	public List<Topic> getTrendingTopics(){
-		//TODO
-//		final List<Topic> trendingTopics = new ArrayList<Topic>();
-//		final Topic topic = new Topic();
-//		topic.setName("Topic de pruebaaaaaaaaaaa!");
-//		trendingTopics.add(topic);
-		List<String> socialNets = new ArrayList<String>();
-		socialNets.add("twitter");
-		socialNets.add("youtube");
-		return trendingTopicCollector.getTrendingTopics(socialNets);
+	public List<Topic> getTrendingTopics(
+			@RequestParam(value="socialnets", required=true) String socialnets,
+			@RequestParam(value="type", required=false) String type,
+			@RequestParam(value="place", required=false) String place
+			){
+		final List<String> socialNets = Arrays.asList(socialnets.split(","));
+		return trendingTopicCollector.getTrendingTopics(socialNets, type, place);
 	}
 
-	@RequestMapping(path="/trendingtopicname/{type}")
+	@RequestMapping(path="/trendingtopicname")
 	public String getTrendingTopicName(
-			@PathVariable(value="type") String type,
-			@RequestParam(value="place", required=false) String place){		
+			@RequestParam(value="type", required=false) String type,
+			@RequestParam(value="place", required=false) String place){
+		//TODO: El closest depende de la ubicación del servidor del api? comprobar!
 		return 
-			TP_CLOSEST_REQUEST_TYPE.equalsIgnoreCase(type) ?  trendingTopicCollector.getClosestTrendingTopicName() :
-			TP_WORLD_REQUEST_TYPE.equalsIgnoreCase(type) ? trendingTopicCollector.getWorldTrendingTopicName() :
+			Constants.TP_CLOSEST_REQUEST_TYPE.equalsIgnoreCase(type) ?  trendingTopicCollector.getClosestTrendingTopicName() :
+			Constants.TP_WORLD_REQUEST_TYPE.equalsIgnoreCase(type) ? trendingTopicCollector.getWorldTrendingTopicName() :
 				trendingTopicCollector.getTrendingTopicNameByPlace(place);
 	}
 
